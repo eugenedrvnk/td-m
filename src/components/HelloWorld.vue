9ec8ec79d1cbabcccce671eba37ae00736940fa2
oziged <oziged@gmail.com>
2020-04-28 16:29:37 +0300

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/> -->
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log(this.$store.dispatch('fetchUsers', 'https://randomuser.me/api/?results=20'))
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
